ad32bc997565c5008ea92bf7feb60b4d34e47818
taenaratae <151068824+taenaratae@users.noreply.github.com>
2023-11-26 21:28:36 +0900

    Update data_path.v

--- data_path.v
+++ data_path.v
@@ -7,10 +7,9 @@ module data_path(
     output W, go, C
     );
     
-    wire same_result;
     reg [2-1:0] T;
     
-    check_same cs(.clk(clk), .position_data(position_data), .A(A), .result(same_result));
+    check_same cs(.clk(clk), .position_data(position_data), .A(A), .result(go));
     check_win cw(.clk(clk), .T(T), .B(B), .W(W), .afterposition_data(afterposition_data));
     next_turn nw(.clk(clk), .T(T), .N(N), .statecombo_next_turn(statecombo_next_turn), .result(next_turn_result));
     
