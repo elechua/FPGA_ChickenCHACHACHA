250b9a22ddfeeb2b52e03194528ef28c4a50a71d
taenaratae <151068824+taenaratae@users.noreply.github.com>
2023-12-01 02:15:46 +0900

    Update cnt_player1.v

--- cnt_player1.v
+++ cnt_player1.v
@@ -8,16 +8,10 @@ module cnt_player1(
         if (rst) begin
             if(N==2)
                 p1_cnt=5'b00000;
-                p2_cnt=5'b01100;
             else if(N==3)
                 p1_cnt=5'b00000;
-                p2_cnt=5'b01000;
-                p3_cnt=5'b10000;
             else if(N==4)
                 p1_cnt=5'b00000;
-                p1_cnt=5'b00110;
-                p1_cnt=5'b01100;
-                p1_cnt=5'b11000;
         end
         else if (p_da1 == 1) begin 
             if (p1_cnt <= 22)
