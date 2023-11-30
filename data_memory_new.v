57d88d512c17f65da508fa031595a4908a0f386a
taenaratae <151068824+taenaratae@users.noreply.github.com>
2023-12-01 01:50:04 +0900

    Create data_memory_new.v

---
+++ data_memory_new.v
@@ -0,0 +1,13 @@
+module data_memory_new(
+    input clk, WR,
+    input [4-1:0] ADDR,
+    input [120-1:0] EDGE_DATA_IN,
+    input [4-1:0] DATA_OUT
+    );
+    reg [4-1:0] SRAM [16-1:0];
+    
+    always @ (posedge clk) begin
+        if (WR) SRAM[ADDR] <= DATA_IN;
+    end
+    assign DATA_OUT = SRAM[ADDR];
+endmodule
