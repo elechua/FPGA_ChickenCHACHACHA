5ef5a01510c6379a9415220195ffb97e991ede5e
taenaratae <151068824+taenaratae@users.noreply.github.com>
2023-12-01 02:20:43 +0900

    Update check_win.v

--- check_win.v
+++ check_win.v
@@ -8,15 +8,21 @@ module check_win(
     
     wire da1, da2, da3, da4;
     wire stop_increase;
-    wire [5-1:0] p1_cnt, p2_cnt, p3_cnt;
+    wire [5-1:0] p1_cnt, p2_cnt, p3_cnt, p4_cnt;
     wire [5-1:0] out1, out2;
     wire [5-1:0] pos_data;
+    wire stop_increase_1, stop_increase_2, stop_increase_3, stop_increase_4;
+    
+    assign stop_increase_1 = da1 & B;
+    assign stop_increase_2 = da2 & B;
+    assign stop_increase_3 = da3 & B;
+    assign stop_increase_4 = da4 & B;
     
     decoder decoder(.T(T), .da1(da1), .da2(da2), .da3(da3), .da4(da4));
-    two_to_one_MUX mux(.B(B), .stop_increase(stop_increase));
-    cnt_player1 cnp1(.rst(rst), .B(B), .N(N) .p_da1(da1&stop_incrase), .p1_cnt(p1_cnt));
-    cnt_player2 cnp2(.rst(rst), .B(B), .N(N) .p_da2(da2&stop_incrase), .p2_cnt(p2_cnt));
-    cnt_player3 cnp3(.rst(rst), .B(B), .N(N) .p_da3(da3&stop_incrase), .p3_cnt(p3_cnt));
+    cnt_player1 cnp1(.rst(rst), .B(B), .N(N) .p_da1(stop_increase_1), .p1_cnt(p1_cnt));
+    cnt_player2 cnp2(.rst(rst), .B(B), .N(N) .p_da2(stop_increase_2), .p2_cnt(p2_cnt));
+    cnt_player3 cnp3(.rst(rst), .B(B), .N(N) .p_da3(stop_increase_3), .p3_cnt(p3_cnt));
+    cnt_player4 cnp4(.rst(rst), .B(B), .N(N) .p_da4(stop_increase_4), .p3_cnt(p3_cnt));
     three_to_one_MUX_upper upmux(.T(T), .p1_cnt(p1_cnt), .p2_cnt(p2_cnt), .p3_cnt(p3_cnt), .out(out1));
     three_to_one_MUX_lower lowmux(.T(T), .p1_cnt(p1_cnt), .p2_cnt(p2_cnt), .p3_cnt(p3_cnt), .out(out2));
     which_one wo(.out1(out1), .out2(out2), .pos_data(pos_data));
