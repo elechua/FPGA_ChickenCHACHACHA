612bd1da0ea5ce1b8dd1ed4d6cb67f7fb6e2c1dd
taenaratae <151068824+taenaratae@users.noreply.github.com>
2023-12-01 02:54:55 +0900

    Update two_to_one_MUX.v

--- two_to_one_MUX.v
+++ two_to_one_MUX.v
@@ -1,11 +1,15 @@
 module two_to_one_MUX(
-    input B,
-    output reg stop_increase
+    input [4-1:0] key,
+    input [5-1:0] tile_info,
+    input A,
+    output reg [4-1:0] ADDR_IN
 );
     always @ (*) begin
-        case(B)
+        case(A)
             1'b0: stop_increase = 0;
             1'b1: stop_increase = 1;
         endcase
     end
+
+    assign ADDR_IN = //비트수가 다른데 어카지
 endmodule
