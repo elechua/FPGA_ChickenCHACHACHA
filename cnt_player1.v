273ab080f9e943bee1443047be4468152feba320
taenaratae <151068824+taenaratae@users.noreply.github.com>
2023-11-30 21:48:57 +0900

    Update cnt_player1.v

--- cnt_player1.v
+++ cnt_player1.v
@@ -4,9 +4,11 @@ module cnt_player1(
     output reg [5-1:0] p1_cnt
 );
     always @ (posedge clk) begin
-        if (p1_cnt <= 22)
-            p1_cnt = p1_cnt + 1;
-        else if (p1_cnt == 23)
-            p1_cnt = 0;    
+        if (p_da1 == 1) { 
+            if (p1_cnt <= 22)
+                p1_cnt = p1_cnt + 1;
+            else if (p1_cnt == 23)
+                p1_cnt = 0;
+        }
     end
 endmodule
