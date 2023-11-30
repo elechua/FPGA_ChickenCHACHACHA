9656cf0e410ec9fc8e1318b0ea528a0c1e0527fa
taenaratae <151068824+taenaratae@users.noreply.github.com>
2023-12-01 02:47:19 +0900

    Update data_path.v

--- data_path.v
+++ data_path.v
@@ -4,7 +4,8 @@ module data_path(
     input [2-1:0] N,
     input [4-1:0] position_data,
     input A, B
-    output W, go, C
+    output W, go, C, 
+    output [5-1:0] reg tile_info
     );
     
     reg [2-1:0] T;
@@ -12,6 +13,7 @@ module data_path(
     check_same cs(.clk(clk), .position_data(position_data), .A(A), .result(go));
     check_win cw(.clk(clk), .T(T), .B(B), .N(N), .W(W), .afterposition_data(afterposition_data));
     next_turn nw(.rst(rst), .T(T), .N(N), .statecombo_next_turn(statecombo_next_turn), .result(next_turn_result));
-    
+
+    assign tile_info = afterposition_data;
     
 endmodule
