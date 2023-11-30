b96d2471a9b7345ef7dd6d64903ae5f5274f8be3
taenaratae <151068824+taenaratae@users.noreply.github.com>
2023-12-01 02:17:21 +0900

    Update check_win.v

--- check_win.v
+++ check_win.v
@@ -14,9 +14,9 @@ module check_win(
     
     decoder decoder(.T(T), .da1(da1), .da2(da2), .da3(da3), .da4(da4));
     two_to_one_MUX mux(.B(B), .stop_increase(stop_increase));
-    cnt_player1 cnp1(.rst(rst), .B(B), .p_da1(da1&stop_incrase), .p1_cnt(p1_cnt));
-    cnt_player2 cnp2(.rst(rst), .B(B), .p_da2(da2&stop_incrase), .p2_cnt(p2_cnt));
-    cnt_player3 cnp3(.rst(rst), .B(B), .p_da3(da3&stop_incrase), .p3_cnt(p3_cnt));
+    cnt_player1 cnp1(.rst(rst), .B(B), .N(N) .p_da1(da1&stop_incrase), .p1_cnt(p1_cnt));
+    cnt_player2 cnp2(.rst(rst), .B(B), .N(N) .p_da2(da2&stop_incrase), .p2_cnt(p2_cnt));
+    cnt_player3 cnp3(.rst(rst), .B(B), .N(N) .p_da3(da3&stop_incrase), .p3_cnt(p3_cnt));
     three_to_one_MUX_upper upmux(.T(T), .p1_cnt(p1_cnt), .p2_cnt(p2_cnt), .p3_cnt(p3_cnt), .out(out1));
     three_to_one_MUX_lower lowmux(.T(T), .p1_cnt(p1_cnt), .p2_cnt(p2_cnt), .p3_cnt(p3_cnt), .out(out2));
     which_one wo(.out1(out1), .out2(out2), .pos_data(pos_data));
