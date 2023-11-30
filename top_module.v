fa4bc59da248caa57264a72073154843d5e3e06e
taenaratae <151068824+taenaratae@users.noreply.github.com>
2023-12-01 02:48:00 +0900

    Update top_module.v

--- top_module.v
+++ top_module.v
@@ -3,7 +3,7 @@ module top_module(
     );
     
     control_unit cu(.clk(clk), .rst(rst), .c(C), .key(key), .go(go), .win(W), .M(M), .A(A), .B(B), .N(N), .M(M));
-    data_path dp(.clk(clk), .statecombo_next_turn(statecombo_next_turn), .N(N), .position_data(position_data), .A(A), .B(B), .W(W), .go(go), .C(C));
+    data_path dp(.clk(clk), .statecombo_next_turn(statecombo_next_turn), .N(N), .position_data(position_data), .A(A), .B(B), .W(W), .go(go), .C(C), .tile_infor(tile_info));
     data_memory dm(); //
     random_generator rg(); // ��
 endmodule
