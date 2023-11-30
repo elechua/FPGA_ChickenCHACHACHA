5437065cb4c1e1f9747d5c1e00b4d9a30443296c
taenaratae <151068824+taenaratae@users.noreply.github.com>
2023-11-30 22:16:13 +0900

    Update next_turn.v

--- next_turn.v
+++ next_turn.v
@@ -4,7 +4,18 @@ module next_turn(
     input [2-1:0] T,
     input statecombo_next_turn,
     output [2-1:0] result
-    // Q가 100에서 101
+    // Q가 100에서 101으로 바뀌는 때가 posedge인가?
+
+    always @ (posedge clk) begin
+        case (N)
+            2'b00: if (statecombo_next_turn == 1)
+                if (T == 2'b00) T = 2'b01;
+                else if (T == 2'b01) T = 2'b00;
+            2'b01: if (statecombo_next_turn == 1)
+            2'b10: if (statecombo_next_turn == 1)
+        endcase
+    end
+    
     // Q가 101에서 110으로 바뀌는 때가
     );
 endmodule
