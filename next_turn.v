3b50df9931d3c03cdce2234975e1134865aed8ed
taenaratae <151068824+taenaratae@users.noreply.github.com>
2023-12-01 02:35:54 +0900

    Update next_turn.v

--- next_turn.v
+++ next_turn.v
@@ -9,17 +9,26 @@ module next_turn(
     always @ (posedge statecombo_next_turn or posedge rst) begin
         if (rst) T = 2'b0;
         else begin
-        case (N)
-            2'b00: if (statecombo_next_turn == 1) begin
-                if (T == 2'b00) T = 2'b01;
-                else if (T == 2'b01) T = 2'b00;
-            end
-            2'b01: if (statecombo_next_turn == 1)
-            2'b10: if (statecombo_next_turn == 1)
-        endcase
+            case (N)
+                2'b00: if (statecombo_next_turn == 1) begin
+                    if (T == 2'b00) T = 2'b01;
+                    else if (T == 2'b01) T = 2'b00;
+                end
+                2'b01: if (statecombo_next_turn == 1) begin
+                    if (T == 2'b00) T = 2'b01;
+                    else if (T == 2'b01) T = 2'b10;
+                    else if (T == 2'b10) T = 2'b00;
+                end
+                2'b10: if (statecombo_next_turn == 1) begin
+                    if (T == 2'b00) T = 2'b01;
+                    else if (T == 2'b01) T = 2'b10;
+                    else if (T == 2'b10) T = 2'b11;
+                    else if (T == 2'b11) T = 2'b00;
+                end
+            endcase
         end
     end
-    
+    assign result = T;
     // Q가 101에서 110으로 바뀌는 때가
     );
 endmodule
