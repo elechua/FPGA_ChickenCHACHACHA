296caf469a621607172ea8606bb8cca9b0bf6d59
taenaratae <151068824+taenaratae@users.noreply.github.com>
2023-11-30 22:01:28 +0900

    Update which_one.v

--- which_one.v
+++ which_one.v
@@ -5,8 +5,12 @@ module which_one(
     always @ (*) begin
         if (out2 - out1 == 1)
             pos_data = out_1 + 2;
-        else if (out1==5'd23 && out2==5'd0))
-            pos_data = 5'd1;
+        else if (out1==5'd23) { 
+            if (out2 == 5'd0)
+                pos_data = 5'd1;
+            else
+                pos_data = 5'd0;  
+        }
         else
             pos_data = out_1 + 1;
     end
