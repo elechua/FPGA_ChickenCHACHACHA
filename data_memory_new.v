4c185dc2aa847a2ff6415cd42434876f9854ee5e
taenaratae <151068824+taenaratae@users.noreply.github.com>
2023-12-01 20:36:17 +0900

    Update data_memory_new.v

--- data_memory_new.v
+++ data_memory_new.v
@@ -6,6 +6,10 @@ module data_memory_new(
     input [4-1:0] DATA_OUT
     );
     reg [4-1:0] SRAM [16-1:0];
+    inital begin
+
+    end
+
     
     always @ (posedge clk) begin
         if (WR) SRAM[ADDR] <= DATA_IN;
