b268a69e8edb3b695d990eeecef71ec42cd81af6
taenaratae <151068824+taenaratae@users.noreply.github.com>
2023-12-01 20:35:38 +0900

    Update data_memory_new.v

--- data_memory_new.v
+++ data_memory_new.v
@@ -2,6 +2,7 @@ module data_memory_new(
     input clk, WR,
     input [4-1:0] ADDR,
     input [120-1:0] EDGE_DATA_IN,
+    input [60-1:0] CENTER_DATA_IN,
     input [4-1:0] DATA_OUT
     );
     reg [4-1:0] SRAM [16-1:0];
