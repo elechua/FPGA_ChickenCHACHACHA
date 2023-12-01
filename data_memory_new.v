5044ee338543ad70470abbec9f58de98accc8981
taenaratae <151068824+taenaratae@users.noreply.github.com>
2023-12-01 20:57:37 +0900

    Update data_memory_new.v

--- data_memory_new.v
+++ data_memory_new.v
@@ -6,13 +6,10 @@ module data_memory_new(
     input [4-1:0] DATA_OUT
     );
     reg [4-1:0] SRAM [36-1:0];
-    inital begin
-        SRAM[0] = EDGE_DATA_IN[95:92];
-    end
-
-    
+   
     always @ (posedge clk) begin
-        if (WR) SRAM[ADDR] <= DATA_IN;
+        if (WR)
+            SRAM[0] = EDGE_DATA_IN[95:92];
     end
     assign DATA_OUT = SRAM[ADDR];
 endmodule
