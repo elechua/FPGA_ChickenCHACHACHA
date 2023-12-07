bfebac46d4ab940b4711a2f24c1f52d1d6011a83
taenaratae <151068824+taenaratae@users.noreply.github.com>
2023-12-07 20:04:10 +0900

    Update top_module.v

--- top_module.v
+++ top_module.v
@@ -7,7 +7,8 @@ module top_module(
     two_to_one_MUX ttom(.key(key), .tile_info(tile_info), .A(A), .ADDR_IN(ADDR_IN)); //이거 수정 필요함..
     data_memory dm(.clk(clk), .WR(WR), .ADDR(ADDR_IN), .DATA_IN(DATA_IN), .DATA_OUT(DATA_OUT)); //얘도 아직..
     random_generator rg(.random_edge_order(random_edge_order), .random_center_order(random_center_order)); //얘도..
-    display_module dm(.clk(clk), .rst(rst), .edge_order(random_edge_order), .center_order(random_center_order), .P1_pos(), .P2_pos(), .P3_pos(), P4_pos(), .den(den), .hsync(hsync), .vsync(vsync), .R(R), .G(G), .B(B), .dclk(dclk), .disp_en(disp_en));
+    display_module dm(.clk(clk), .rst(rst), .edge_order(random_edge_order), .center_order(random_center_order), .P1_pos(), .P2_pos(), .P3_pos(), P4_pos(),
+                      .den(den), .hsync(hsync), .vsync(vsync), .R(R), .G(G), .B(B), .dclk(dclk), .disp_en(disp_en));
     
     
 endmodule
