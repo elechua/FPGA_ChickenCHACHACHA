c5624a224a93d4728d0fdbb0a3607e5f1ff94a14
taenaratae <151068824+taenaratae@users.noreply.github.com>
2023-12-08 00:18:11 +0900

    Update cnt_player4.v

--- cnt_player4.v
+++ cnt_player4.v
@@ -1,23 +1,23 @@
 module cnt_player4(
-    input B, rst,
-    input [5-1:0] N,
+    input B,
+    input [2-1:0] N,
     input p_da4,
     output reg [5-1:0] p4_cnt
 );
-    always @ (posedge B or posedge rst) begin
-        if (rst) begin
-            if(N==2)
-                p4_cnt=5'b00000;
-            else if(N==3)
-                p4_cnt=5'b00000;
-            else if(N==4)
-                p4_cnt=5'b11000;
-        end
-        else if (p_da4 == 1) begin 
-             if (p4_cnt <= 22)
-                p4_cnt = p4_cnt + 1;
-            else if (p4_cnt == 23)
-                p4_cnt = 0;
+    always @ (N) begin
+        if(N == 2'd0)
+            p4_cnt = 5'b00000;
+        else if(N == 2'd1)
+            p4_cnt = 5'b00000;
+        else if(N == 2'd2)
+            p4_cnt = 5'b11000;
+    end
+    always @ (posedge B) begin
+        if (p_da4 == 1) begin 
+            if (p4_cnt <= 5'd22)
+                p4_cnt = p4_cnt + 5'd1;
+            else if (p4_cnt == 5'd23)
+                p4_cnt = 5'd0;
         end
     end
 endmodule
