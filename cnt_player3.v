8d2d490e276808ac84ec49aaf389ee0856dd8e9f
taenaratae <151068824+taenaratae@users.noreply.github.com>
2023-12-08 00:18:01 +0900

    Update cnt_player3.v

--- cnt_player3.v
+++ cnt_player3.v
@@ -1,23 +1,23 @@
 module cnt_player3(
-    input B, rst,
-    input [5-1:0] N,
+    input B,
+    input [2-1:0] N,
     input p_da3,
     output reg [5-1:0] p3_cnt
 );
-    always @ (posedge B or posedge rst) begin
-        if (rst) begin
-            if(N==2)
-                p3_cnt=5'b00000;
-            else if(N==3)
-                p3_cnt=5'b10000;
-            else if(N==4)
-                p3_cnt=5'b01100;
-        end
-        else if (p_da3 == 1) begin 
-            if (p3_cnt <= 22)
-                p3_cnt = p3_cnt + 1;
-            else if (p3_cnt == 23)
-                p3_cnt = 0;
+    always @ (N) begin
+        if(N == 2'd0)
+            p3_cnt = 5'b00000;
+        else if(N == 2'd1)
+            p3_cnt = 5'b10000;
+        else if(N == 2'd2)
+            p3_cnt = 5'b01100;
+    end
+    always @ (posedge B) begin
+        if (p_da3 == 1) begin 
+            if (p3_cnt <= 5'd22)
+                p3_cnt = p3_cnt + 5'd1;
+            else if (p3_cnt == 5'd23)
+                p3_cnt = 5'd0;
         end
     end
 endmodule
