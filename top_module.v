01b91d8419cceab16fc8c76e9b3846b9ef88f12c
taenaratae <151068824+taenaratae@users.noreply.github.com>
2023-12-07 21:13:56 +0900

    Update top_module.v

--- top_module.v
+++ top_module.v
@@ -12,7 +12,7 @@ module top_module(
         data_path dp(.clk(clk), .rst(rst), .statecombo_next_turn(statecombo_next_turn), .N(N), .position_data(position_data), .A(A), .B(B), .W(W), .go(go), .C(C), .tile_infor(tile_info));
         two_to_one_MUX ttom(.key(key), .tile_info(tile_info), .A(A), .ADDR_IN(ADDR_IN)); //이거 수정 필요함..
         data_memory dm(.clk(clk), .WR(WR), .ADDR(ADDR_IN), .EDGE_DATA_IN(random_edge_order), .CENTER_DATA_IN(random_center_order), .DATA_OUT(DATA_OUT));
-        random_generator rg(.rst(rst), .random_edge_order(random_edge_order), .random_center_order(random_center_order)); //얘도..
+        random_generator rg(.rst(rst), .random_edge_order(random_edge_order), .random_center_order(random_center_order));
         display_module dm(.clk(clk), .rst(rst), .edge_order(random_edge_order), .center_order(random_center_order), .P1_pos(), .P2_pos(), .P3_pos(), P4_pos(),
                         .den(den), .hsync(hsync), .vsync(vsync), .R(R), .G(G), .B(B), .dclk(dclk), .disp_en(disp_en));
         keypad key(.clk(clk), .rst(rst), .key_col(key_col), .key_row(key_row), .key_value(key)); // 코드 맞음
