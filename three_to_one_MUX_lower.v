d74c16e92913bb3e8f9bcff6bb7034627a416c7a
taenaratae <151068824+taenaratae@users.noreply.github.com>
2023-12-07 22:24:15 +0900

    Update three_to_one_MUX_lower.v

--- three_to_one_MUX_lower.v
+++ three_to_one_MUX_lower.v
@@ -1,13 +1,32 @@
 module three_to_one_MUX_lower(
     input [2-1:0] T,
+    input [2-1:0] N,
     input [5-1:0] p1_cnt, p2_cnt, p3_cnt,
     output reg [5-1:0] out
 );
     always @ (*) begin
-        case (T)
-            2'b00: out = p2_cnt;
-            2'b01: out = p3_cnt;
-            2'b10: out = p1_cnt;
+        case (N)
+            2'b00: begin
+                case (T)
+                    2'b00: out = p2_cnt;
+                    2'b01: out = p1_cnt;
+                endcase
+            end
+            2'b01: begin
+                case (T)
+                    2'b00: out = p2_cnt;
+                    2'b01: out = p3_cnt;
+                    2'b10: out = p1_cnt;
+                endcase
+            end
+            2'b10: begin
+                case (T)
+                    2'b00: out = p2_cnt;
+                    2'b01: out = p3_cnt;
+                    2'b10: out = p4_cnt;
+                    2'b11: out = p1_cnt;
+                endcase
+            end  
         endcase
     end
 endmodule
