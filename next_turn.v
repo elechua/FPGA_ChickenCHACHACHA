1482d3f72a1b307cdd1f4420bb1757807327a4b8
taenaratae <151068824+taenaratae@users.noreply.github.com>
2023-12-08 00:25:28 +0900

    Update next_turn.v

--- next_turn.v
+++ next_turn.v
@@ -4,30 +4,32 @@ module next_turn(
     input [2-1:0] T,
     input statecombo_next_turn,
     output [2-1:0] result
-)
+    );
     // Q가 100에서 101으로 바뀌는 때가 posedge인가?
+    reg [2-1:0] t;
+    
     always @ (posedge statecombo_next_turn or posedge rst) begin
-        if (rst) T = 2'b0;
+        if (rst) t = 2'b0;
         else begin
             case (N)
                 2'b00: if (statecombo_next_turn == 1) begin
-                    if (T == 2'b00) T = 2'b01;
-                    else if (T == 2'b01) T = 2'b00;
+                    if (t == 2'b00) t = 2'b01;
+                    else if (t == 2'b01) t = 2'b00;
                 end
                 2'b01: if (statecombo_next_turn == 1) begin
-                    if (T == 2'b00) T = 2'b01;
-                    else if (T == 2'b01) T = 2'b10;
-                    else if (T == 2'b10) T = 2'b00;
+                    if (t == 2'b00) t = 2'b01;
+                    else if (t == 2'b01) t = 2'b10;
+                    else if (t == 2'b10) t = 2'b00;
                 end
                 2'b10: if (statecombo_next_turn == 1) begin
-                    if (T == 2'b00) T = 2'b01;
-                    else if (T == 2'b01) T = 2'b10;
-                    else if (T == 2'b10) T = 2'b11;
-                    else if (T == 2'b11) T = 2'b00;
+                    if (t == 2'b00) t = 2'b01;
+                    else if (t == 2'b01) t = 2'b10;
+                    else if (t == 2'b10) t = 2'b11;
+                    else if (t == 2'b11) t = 2'b00;
                 end
             endcase
         end
     end
-    assign result = T;
+    assign result = t;
     // Q가 101에서 110으로 바뀌는 때가
 endmodule
