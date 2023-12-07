0d3ad5725e1255db7e0fcf832f95cb6834f192f9
taenaratae <151068824+taenaratae@users.noreply.github.com>
2023-12-08 00:38:31 +0900

    Update next_turn.v

--- next_turn.v
+++ next_turn.v
@@ -1,7 +1,7 @@
 module next_turn(
     input rst,
     input [2-1:0] N,
-    input [2-1:0] T,
+    //input [2-1:0] T,
     input statecombo_next_turn,
     output [2-1:0] result
     );
@@ -13,19 +13,19 @@ module next_turn(
         else begin
             case (N)
                 2'b00: if (statecombo_next_turn == 1) begin
-                    if (T == 2'b00) t = 2'b01;
-                    else if (T == 2'b01) t = 2'b00;
+                    if (t == 2'b00) t = 2'b01;
+                    else if (t == 2'b01) t = 2'b00;
                 end
                 2'b01: if (statecombo_next_turn == 1) begin
-                    if (T == 2'b00) t = 2'b01;
-                    else if (T == 2'b01) t = 2'b10;
-                    else if (T == 2'b10) t = 2'b00;
+                    if (t == 2'b00) t = 2'b01;
+                    else if (t == 2'b01) t = 2'b10;
+                    else if (t == 2'b10) t = 2'b00;
                 end
                 2'b10: if (statecombo_next_turn == 1) begin
-                    if (T == 2'b00) t = 2'b01;
-                    else if (T == 2'b01) t = 2'b10;
-                    else if (T == 2'b10) t = 2'b11;
-                    else if (T == 2'b11) t = 2'b00;
+                    if (t == 2'b00) t = 2'b01;
+                    else if (t == 2'b01) t = 2'b10;
+                    else if (t == 2'b10) t = 2'b11;
+                    else if (t == 2'b11) t = 2'b00;
                 end
             endcase
         end
