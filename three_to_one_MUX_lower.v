e4461fa892d58f385dd5887a0218d29ed4e7d7e0
taenaratae <151068824+taenaratae@users.noreply.github.com>
2023-12-07 22:57:46 +0900

    Update three_to_one_MUX_lower.v

--- three_to_one_MUX_lower.v
+++ three_to_one_MUX_lower.v
@@ -4,7 +4,7 @@ module three_to_one_MUX_lower(
     input [5-1:0] p1_cnt, p2_cnt, p3_cnt, p4_cnt,
     output reg [5-1:0] out
 );
-    always @ (*) begin
+    always @ (p1_cnt or p2_cnt or p3_cnt or p4_cnt) begin
         case (N)
             2'b00: begin
                 case (T)
