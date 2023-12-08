b414c870a1366b8d26f844a0cf5a48244037b12b
elechua <148829845+elechua@users.noreply.github.com>
2023-12-08 10:05:58 +0900

    Create N_DFF.v

---
+++ N_DFF.v
@@ -0,0 +1,14 @@
+module N_DFF(
+  input [4-1:0]key,
+  input [3-1:0]M,
+  output [2-1:0]N
+);
+
+  always @ (M or key) begin
+    if( M == 001 )
+      N <= key;
+    else
+      N <= N;
+  end
+
+endmodule
