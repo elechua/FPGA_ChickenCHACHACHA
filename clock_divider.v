8e7fcc9092321bd87fbc4497db73978e1486861e
taenaratae <151068824+taenaratae@users.noreply.github.com>
2023-12-12 17:43:54 +0900

    Update clock_divider.v

--- clock_divider.v
+++ clock_divider.v
@@ -2,17 +2,17 @@ module clk_oscillator(
     input clk, rst,
     output reg clk_keypad
     );
-    reg [4-1:0] cnt;
+    reg [5-1:0] cnt;
     
     always @ (posedge clk) begin
         if (rst) begin
-            cnt = 4'd0;
+            cnt = 5'd0;
             clk_keypad = 0;
         end
-        else if (cnt >= 4'd25) begin
+        else if (cnt >= 5'd25) begin
             clk_keypad = ~clk_keypad;
-            cnt = 4'd0;
+            cnt = 5'd0;
         end
-        else cnt = cnt + 4'd1;
+        else cnt = cnt + 5'd1;
     end
 endmodule
