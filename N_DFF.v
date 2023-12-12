b596ae571c8c55056a054cba12bd9525c962fdb5
taenaratae <151068824+taenaratae@users.noreply.github.com>
2023-12-13 00:19:09 +0900

    Update N_DFF.v

--- N_DFF.v
+++ N_DFF.v
@@ -1,11 +1,12 @@
 module N_DFF(
-  input [4-1:0] key,
-  input [3-1:0] M,
-  output reg [2-1:0] N
+    input clk,
+    input [4-1:0] key,
+    input [3-1:0] M,
+    output reg [3-1:0] N
 );
 
-  always @ (M or key) begin
-    if( M == 001 )
+  always @ (posedge clk) begin
+    if(M == 001)
       N <= key;
     else
       N <= N;
