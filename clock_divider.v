ee82d36d069d35e9e22cf6883df9777e3435684f
taenaratae <151068824+taenaratae@users.noreply.github.com>
2023-12-12 17:39:25 +0900

    Update clock_divider.v

--- clock_divider.v
+++ clock_divider.v
@@ -1,33 +1,16 @@
-module clk_divider(
+module clk_oscillator(
     input clk, rst,
-    output LED1, 
-    output reg LED2, LED3
+    output clk_keypad
     );
-    reg [4-1:0] cnt2;
-    reg [7-1:0] cnt3;
-    assign LED1 = clk;
+    reg [4-1:0] cnt;
     
     always @ (posedge clk) begin
         if (rst) begin
-            cnt2 = 4'd0;
-            LED2 = 0;
+            cnt = 4'd0;
         end
-        else if (cnt2 >= 4'd5) begin
-            LED2 = ~LED2;
-            cnt2 = 4'd0;
+        else if (cnt2 >= 4'd25) begin
+            cnt = 4'd0;
         end
-        else cnt2 = cnt2 + 4'd1;
-    end
-    
-    always @ (posedge clk) begin
-        if (rst) begin
-            cnt3 = 6'd0;
-            LED3 = 0;
-        end
-        else if (cnt3 >= 6'd50) begin
-            LED3 = ~LED3;
-            cnt3 = 6'd0;
-        end
-        else cnt3 = cnt3 + 6'd1;
+        else cnt = cnt + 4'd1;
     end
 endmodule
